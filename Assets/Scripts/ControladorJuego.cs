4c88d89ca82d5ab0495959df078dc6b756bf52e7
Froznar <francisco.marin@ucsp.edu.pe>
2017-04-01 01:48:22 -0500

    sprites de estrellas

--- Assets/Scripts/ControladorJuego.cs
+++ Assets/Scripts/ControladorJuego.cs
@@ -11,28 +11,40 @@ public class ControladorJuego : MonoBehaviour
 
 	public GameObject pelota;
 	private Animator animador;
+    /*public Image star1;
+    public Image star2;
+    public Image star3;
+    public Color redd;*/
 
-	private bool ganar;
+    private bool ganar;
 	private bool perder;
 	// Use this for initialization
 	void Start () 
 	{
 		pelota = GameObject.FindGameObjectWithTag ("Player");
 		animador = GetComponent<Animator> ();
+        //star1 = GameObject.Find("star1").GetComponent<Image>();
+        //star2 = GameObject.Find("star2").GetComponent<Image>();
+        //star3 = GameObject.Find("star3").GetComponent<Image>();
+        //redd = new Color(159, 111, 111);
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		ganar = pelota.GetComponent<Pelota> ().ganar;
 		perder = pelota.GetComponent<Pelota> ().perder;
-
-		puntuacion = pelota.GetComponent<Pelota> ().puntuacion;
+        puntuacion = pelota.GetComponent<Pelota> ().puntuacion;
 		tiempo = pelota.GetComponent<Pelota> ().tempo;
+        
 
-		textoPuntuacion.text = "Puntuacion: " + puntuacion;
-		textoTiempo.text = "Tiempo: " + Mathf.Floor(tiempo);
+        /*if (tiempo > 5)
+        {
+            star1.color = redd;
+        }*/
+         
+        textoTiempo.text = "Tiempo: " + Mathf.Floor(tiempo);
 
 		animador.SetBool ("Ganar", ganar);
 		animador.SetBool ("Perder", perder);
